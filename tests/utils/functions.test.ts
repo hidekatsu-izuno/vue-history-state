c7d369c6c068984a558a8467555b2c679a8f4eec
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2022-10-07 23:35:52 +0900

    Add tests

--- tests/utils/functions.test.ts
+++ tests/utils/functions.test.ts
@@ -1,11 +1,141 @@
 import { isObjectEqual, isObjectMatch } from '../../src/utils/functions'
 
-describe('test functions', () => {
-  it('test isObjectEqual', () => {
+describe('isObjectEqual', () => {
+  test('compare undefineds', () => {
+    expect(isObjectEqual(undefined, undefined)).toBe(true)
+  })
+
+  test('compare nulls', () => {
     expect(isObjectEqual(null, null)).toBe(true)
   })
 
-  it('test isObjectMatch', () => {
+  test('compare empty objects', () => {
+    expect(isObjectEqual({}, {})).toBe(true)
+  })
+
+  test('compare object with empty object', () => {
+    expect(isObjectEqual(
+      { a: null },
+      {},
+    )).toBe(false)
+  })
+
+  test('compare empty object with object', () => {
+    expect(isObjectEqual(
+      {},
+      { a: null },
+    )).toBe(false)
+  })
+
+  test('compare objects', () => {
+    expect(isObjectEqual(
+      { a: null },
+      { a: null },
+    )).toBe(true)
+  })
+
+  test('compare objects', () => {
+    expect(isObjectEqual(
+      { a: null },
+      { a: undefined },
+    )).toBe(false)
+  })
+
+  test('compare objects', () => {
+    expect(isObjectEqual(
+      { a: undefined },
+      { a: null },
+    )).toBe(false)
+  })
+
+  test('compare nested objects', () => {
+    expect(isObjectEqual(
+      { a: 1, b: { c: "2", d: [true, false] }},
+      { a: 1, b: { c: "2", d: [true, false] }},
+    )).toBe(true)
+  })
+
+  test('compare nested objects', () => {
+    expect(isObjectEqual(
+      { a: 1, b: { c: "2", d: [true, false] }},
+      { a: 1, b: { c: "2", d: [true] }},
+    )).toBe(false)
+  })
+
+  test('compare nested objects', () => {
+    expect(isObjectEqual(
+      { a: 1, b: { c: "2", d: [true] }},
+      { a: 1, b: { c: "2", d: [true, false] }},
+    )).toBe(false)
+  })
+})
+
+describe('isObjectMatch', () => {
+  test('compare undefineds', () => {
+    expect(isObjectMatch(undefined, undefined)).toBe(true)
+  })
+
+  test('compare nulls', () => {
     expect(isObjectMatch(null, null)).toBe(true)
+  })
+
+  test('compare empty objects', () => {
+    expect(isObjectMatch({}, {})).toBe(true)
+  })
+
+  test('compare object with empty object', () => {
+    expect(isObjectMatch(
+      { a: null },
+      {},
+    )).toBe(true)
+  })
+
+  test('compare empty object with object', () => {
+    expect(isObjectMatch(
+      {},
+      { a: null },
+    )).toBe(false)
+  })
+
+  test('compare objects', () => {
+    expect(isObjectMatch(
+      { a: null },
+      { a: null },
+    )).toBe(true)
+  })
+
+  test('compare objects', () => {
+    expect(isObjectMatch(
+      { a: null },
+      { a: undefined },
+    )).toBe(false)
+  })
+
+  test('compare objects', () => {
+    expect(isObjectMatch(
+      { a: undefined },
+      { a: null },
+    )).toBe(false)
+  })
+
+  test('compare nested objects', () => {
+    expect(isObjectMatch(
+      { a: 1, b: { c: "2", d: [true, false] }},
+      { a: 1, b: { c: "2", d: [true, false] }},
+    )).toBe(true)
+  })
+
+  test('compare nested objects', () => {
+    expect(isObjectMatch(
+      { a: 1, b: { c: "2", d: [true, false] }},
+      { a: 1, b: { c: "2", d: [true] }},
+    )).toBe(true)
+  })
+
+  test('compare nested objects', () => {
+    expect(isObjectMatch(
+      { a: 1, b: { c: "2", d: [true] }},
+      { a: 1, b: { c: "2", d: [true, false] }},
+    )).toBe(false)
   })
 })
